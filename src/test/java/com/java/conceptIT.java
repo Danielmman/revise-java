b48752c4768a2287a833eaca84b53b15fcd24510
Daniel <danielimmanj@gmail.com>
2023-06-18 21:07:14 +0530

    initial project added

---
+++ src/test/java/com/java/conceptIT.java
@@ -0,0 +1,8 @@
+package com.java;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class conceptIT extends conceptTest {
+    // Execute the same tests but in packaged mode.
+}
